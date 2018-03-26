09597e6efcf01f46a09b33b11f8df5c254e95e20
albarro <alvaroaq@outlook.es>
2018-03-26 21:04:24 +0200

    mas datos de ejemplo

--- src/main/java/com/uniovi/services/InsertSampleDataService.java
+++ src/main/java/com/uniovi/services/InsertSampleDataService.java
@@ -44,5 +44,24 @@ public class InsertSampleDataService {
 		kafkaProducer.send("incidence", incidence1.toJson());
 		kafkaProducer.send("incidence", incidence2.toJson());
 
+		
+		operator1 = new Operator("uo222222", "123456");
+
+		operatorService.addOperator(operator1);
+		
+		Incidence incidence3 = new Incidence("Lectura36", "Lectura humedad", "2513.4562", "normal", "sensor1", "123456", "Sensor");
+		Incidence incidence4 = new Incidence("Lectura37", "Lectura humedad", "2513.4562", "normal", "sensor1", "123456", "Sensor");
+		
+		kafkaProducer.send("incidence", incidence3.toJson());
+		kafkaProducer.send("incidence", incidence4.toJson());
+		
+		
+		incidence3 = new Incidence("normal2", "normal", "", "normal", "Paco", "123456", "Agent");
+		incidence4 = new Incidence("Lectura38", "Lectura humedad", "2513.4562", "normal", "sensor1", "123456", "Sensor");
+		
+		kafkaProducer.send("incidence", incidence3.toJson());
+		kafkaProducer.send("incidence", incidence4.toJson());
+		
+		
 	}
 }
