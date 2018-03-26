b0ba607cc4aed6c82a2a651bffad09e2912f3361
albarro <alvaroaq@outlook.es>
2018-03-26 19:14:02 +0200

    Test Operadores

--- src/test/java/com/uniovi/OperatorTest.java
+++ src/test/java/com/uniovi/OperatorTest.java
@@ -2,6 +2,8 @@ package com.uniovi;
 
 import static org.junit.Assert.*;
 
+import java.util.List;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -10,7 +12,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.uniovi.entities.Incidence;
 import com.uniovi.entities.Operator;
+import com.uniovi.services.IncidenceService;
 import com.uniovi.services.OperatorService;
 import com.uniovi.services.SecurityService;
 
@@ -23,26 +27,37 @@ public class OperatorTest {
 
 	@Autowired
 	private SecurityService securityService;
+	
+	@Autowired
+	private IncidenceService incidenceService;
 
-	private Operator operator1;
-	private Operator operator2;
-	private Operator operator3;
-	private Operator operator4;
-	private Operator operator5;
+	private static Operator operator1;
+	private static Operator operator2;
+	private static Operator operator3;
+	private static Operator operator4;
+	private static Operator operator5;
+	
+	private static boolean setup;
 
 	@Before
-	public void init() {
+	public void setUp() throws Exception {
+		if(!setup) {
+		
 		operator1 = new Operator("op1", "123456");
 		operator2 = new Operator("op2", "123456");
 		operator3 = new Operator("op3", "123456");
 		operator4 = new Operator("op4", "123456");
 		operator5 = new Operator("op5", "123456");
-
+			
 		operatorService.addOperator(operator1);
 		operatorService.addOperator(operator2);
 		operatorService.addOperator(operator3);
 		operatorService.addOperator(operator4);
 		operatorService.addOperator(operator5);
+		
+		
+		setup = true;
+		}
 	}
 
 	@Test
@@ -61,24 +76,74 @@ public class OperatorTest {
 			assertEquals("Bad credentials", e.getMessage());
 		}
 
-		try {
-			securityService.autoLogin("op12", "123456");
-		} catch (Exception e) {
-			assertEquals("Bad credentials", e.getMessage());
-		}
-
 		// Probamos a logearnos con un usuario que no existe
 		try {
 			securityService.autoLogin("op112", "0");
 		} catch (Exception e) {
-			assertEquals("Bad credentials", e.getMessage());
+			assertEquals(NullPointerException.class, e.getClass());
 		}
 
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testFind() {
+		// Probamos a buscar a todos los usuarios de la aplicacion
+		List<Operator> list = operatorService.findAll();
+		assertEquals(6, list.size());
+
+		// Probamos a buscar usuarios concretos
+		assertEquals(operator1.getName(), operatorService.findByName("op1").getName());
+
+		assertEquals(operator3.getName(), operatorService.findByName("op3").getName());
+		
+		assertEquals(operator5.getName(), operatorService.findByName("op5").getName());
+
+		// Probamos a buscar usuarios que no existen
+		assertNull(operatorService.findByName("op12"));
+		
+		assertNull(operatorService.findByName(null));
+	}
+	
+	@Test
+	public void testAssingIncidences() {
+		
+		Incidence incidence1 = new Incidence("normal", "normal", "", "normal", "Paco", "123456", "Agent");
+		Incidence incidence2 = new Incidence("incendio", "incendio en Uría", "", "normal", "Paco", "123456", "Agent");
+		Incidence incidence3 = new Incidence("normal", "normal", "", "normal", "Paco", "123456", "Agent");
+		Incidence incidence4 = new Incidence("incendio", "incendio en Uría", "", "normal", "Paco", "123456", "Agent");
+		Incidence incidence5 = new Incidence("normal", "normal", "", "normal", "Paco", "123456", "Agent");
+		Incidence incidence6 = new Incidence("incendio", "incendio en Uría", "", "normal", "Paco", "123456", "Agent");
+		Incidence incidence7 = new Incidence("normal", "normal", "", "normal", "Paco", "123456", "Agent");
+		
+		incidenceService.addIncidence(incidence1);
+		incidenceService.addIncidence(incidence2);
+		incidenceService.addIncidence(incidence3);
+		incidenceService.addIncidence(incidence4);
+		
+		//Asignamos una incidencia
+		operatorService.assignIncidence(incidence1); //op1
+		
+		//Comprobamos que se asigna
+		assertEquals(operator1.getNumberOfIncidences(), 1);
+		
+		operator2.assignIncidence(incidence2); //op2
+		assertEquals(operator2.getNumberOfIncidences(), 1);
+		
+		operator3.assignIncidence(incidence3); //op3
+		assertEquals(operator3.getNumberOfIncidences(), 1);
+		
+		operator4.assignIncidence(incidence4); //op4
+		assertEquals(operator4.getNumberOfIncidences(), 1);
+		
+		operator5.assignIncidence(incidence5); //op5
+		assertEquals(operator5.getNumberOfIncidences(), 1);
+		
+		// Buscamos el operador con mas incidencias
+		operator1.assignIncidence(incidence6);
+		operator1.assignIncidence(incidence7);
+		
+		assertEquals(operator1, operatorService.findOperatorWithMoreIncidences());
+
 	}
 
 }
