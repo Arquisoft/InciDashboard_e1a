f4b10ae78dc229cbc72a6387d2b891d9276847f0
Angel <uo246998@uniovi.es>
2018-03-26 19:50:39 +0200

    Cambio gitignore

--- src/main/java/com/uniovi/repositories/IncidenceRepository.java
+++ src/main/java/com/uniovi/repositories/IncidenceRepository.java
@@ -12,7 +12,7 @@ import com.uniovi.entities.Incidence;
 @Repository
 public interface IncidenceRepository extends CrudRepository<Incidence, Long>{
 	
-	@Query("SELECT r FROM Incidence r WHERE (LOWER(r.description) LIKE LOWER(?1) OR LOWER(r.name) LIKE LOWER(?1))")
+	@Query("SELECT r FROM Incidence r WHERE (LOWER(r.description) LIKE LOWER(?1) OR LOWER(r.name) LIKE LOWER(?1) OR LOWER(r.etiquetas) LIKE LOWER(?1))")
 			List<Incidence> searchByDescriptionAndName(String seachtext);
 	
 }
