254d4c14bcb03de7ab2757e98447bb1a31c02a5f
Angel <angelpuertadiaz@gmail.com>
2018-05-07 21:57:37 +0200

    Arreglos

--- src/main/java/com/uniovi/controllers/AgentsController.java
+++ src/main/java/com/uniovi/controllers/AgentsController.java
@@ -2,17 +2,12 @@ package com.uniovi.controllers;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import com.uniovi.services.OperatorService;
 
 @Controller
 public class AgentsController {
-
-	@Autowired
-	private OperatorService operatorService;
 
 	private final Logger log = LoggerFactory.getLogger(this.getClass());
 
